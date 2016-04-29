e3a4388010eb59a0a7471a187d8bfcd4196aed22
Kapil Garg <kgarg6@sapient.com>
2016-04-29 14:31:22 -0500

    removing pods from repo
    they will be initialized on clone and setup, don't store them on remote

--- low-effort-sensing/Pods/Target Support Files/Pods-low-effort-sensing/Pods-low-effort-sensing-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_low_effort_sensing : NSObject
-@end
-@implementation PodsDummy_Pods_low_effort_sensing
-@end
--- low-effort-sensing/Pods/Target Support Files/Pretracking/Pretracking-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double PretrackingVersionNumber;
-FOUNDATION_EXPORT const unsigned char PretrackingVersionString[];
-
